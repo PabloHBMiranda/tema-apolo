79397aef437ae1904eb5e14808bb200fd020defe
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-07-24 20:41:38 -0300

    Corrigi as váriaveis e estilos responsivos

--- public/wp-content/themes/tema-apolo/footer.php
+++ public/wp-content/themes/tema-apolo/footer.php
@@ -1,6 +1,6 @@
 <footer>
     dawdawdawdawd
 </footer>
-<script type="text/javascript" src="<?= home_url() ?>/public/wp-content/themes/tema-apolo/bundle/index.js"></script>
+<script type="text/javascript" src="bundle/index.js"></script>
 </body>
 </html>
