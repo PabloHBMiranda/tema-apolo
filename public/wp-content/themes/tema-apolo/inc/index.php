a112025b7085694209ac52e6c2aa30cef9389297
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-08-17 19:48:21 -0300

    Altera a configuração do tema

---
+++ public/wp-content/themes/tema-apolo/inc/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . "/theme/scripts.php";
+require_once __DIR__ . "/theme/settings.php";
+require_once __DIR__ . "/theme/menus.php";
--- public/wp-content/themes/tema-apolo/inc/theme/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . "/scripts.php";
-require_once __DIR__ . "/hocks.php";
-require_once __DIR__ . "/menus.php";
-
-
