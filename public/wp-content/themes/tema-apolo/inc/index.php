6b8efd66a37e9cb083578c2834340d5ad842d202
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-08-29 13:08:48 -0300

    Adiciona a seção do header

--- public/wp-content/themes/tema-apolo/inc/index.php
+++ public/wp-content/themes/tema-apolo/inc/index.php
@@ -2,3 +2,6 @@
 require_once __DIR__ . "/theme/scripts.php";
 require_once __DIR__ . "/theme/settings.php";
 require_once __DIR__ . "/theme/menus.php";
+require_once __DIR__ . "/theme/sidebars.php";
+
+require_once __DIR__ . "/sections/header/header-main.php";
---
+++ public/wp-content/themes/tema-apolo/src/Create_New_Section.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace src;
+
+class Create_New_Section
+{
+
+}
