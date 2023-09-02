cd1f04b357760846444490b4644666b2de3642b5
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-09-02 12:36:47 -0300

    Configura o header bar

---
+++ public/wp-content/themes/tema-apolo/inc/components/social_media.php
@@ -0,0 +1,43 @@
+<?php
+
+$social_midias = [];
+
+if (get_option('apl_admin_sc_facebook')) {
+    $social_midias['facebook'] = [
+        'name' => 'facebook',
+        'url' => get_option('apl_admin_sc_facebook')
+    ];
+}
+
+if (get_option('apl_admin_sc_instagram')) {
+    $social_midias['instagram'] = [
+        'name' => 'instagram',
+        'url' => get_option('apl_admin_sc_instagram')
+    ];
+}
+
+if (get_option('apl_admin_sc_whatsapp')) {
+    $social_midias['whatsapp'] = [
+        'name' => 'whatsapp',
+        'url' => get_option('apl_admin_sc_whatsapp')
+    ];
+}
+
+?>
+
+<?php if (!empty($social_midias)) { ?>
+    <div class="template-part-social-media">
+        <div class="wrapper-social-icons">
+            <?php foreach ($social_midias as $social) { ?>
+                <div class="content-social-media">
+                    <a href="<?= $social['url'] ?>" target="_blank" class="item-social-media">
+                                <span class="icon-social-media">
+                                    <?php include ICONS . $social['name'] . '.php'; ?>
+                                </span>
+                        <p class="text-social-media"> <?= ucfirst($social['name']) ?></p>
+                    </a>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+<?php } ?>
--- public/wp-content/themes/tema-apolo/inc/sections/header/header-bar.php
+++ public/wp-content/themes/tema-apolo/inc/sections/header/header-bar.php
@@ -3,30 +3,6 @@
 $class_name = 'template-section';
 $id = 'header-bar';
 
-$social_midias = [];
-
-if (get_option('apl_admin_sc_facebook')) {
-    $social_midias['facebook'] = [
-        'name' => 'facebook',
-        'url' => get_option('apl_admin_sc_facebook')
-    ];
-}
-
-if (get_option('apl_admin_sc_instagram')) {
-    $social_midias['instagram'] = [
-        'name' => 'instagram',
-        'url' => get_option('apl_admin_sc_instagram')
-    ];
-}
-
-if (get_option('apl_admin_sc_whatsapp')) {
-    $social_midias['whatsapp'] = [
-        'name' => 'whatsapp',
-        'url' => get_option('apl_admin_sc_whatsapp')
-    ];
-}
-
-
 $class_name = [
     $class_name,
     $id
@@ -38,22 +14,7 @@ $class_list = implode('-', $class_name);
 
 <section class="<?= $class_list ?>">
     <div class="container">
-        <?php if (!empty($social_midias)) { ?>
-            <div class="template-part-social-media">
-                <div class="wrapper-social-icons">
-                    <?php foreach ($social_midias as $social) { ?>
-                        <div class="content-social-media">
-                            <a href="<?= $social['url'] ?>" target="_blank" class="item-social-media">
-                                <span class="icon-social-media">
-                                    <?php include ICONS . $social['name'] . '.php'; ?>
-                                </span>
-                                <p class="text-social-media"> <?= ucfirst($social['name']) ?></p>
-                            </a>
-                        </div>
-                    <?php } ?>
-                </div>
-            </div>
-        <?php } ?>
+        <?php get_template_part('inc/components/social_media');?>
     </div>
 </section>
 
