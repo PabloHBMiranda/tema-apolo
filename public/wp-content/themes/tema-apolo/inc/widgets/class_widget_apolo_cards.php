607d11217697a05cd245d1e09027891ee9e83682
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-11-07 19:07:26 -0300

    Adiciona opção de height nos cards

--- public/wp-content/themes/tema-apolo/inc/widgets/class_widget_apolo_cards.php
+++ public/wp-content/themes/tema-apolo/inc/widgets/class_widget_apolo_cards.php
@@ -25,11 +25,13 @@ class Class_Widget_Apolo_Cards extends \WP_Widget{
         $card_title = 'card_title';
         $card_number = 'card_number';
         $card_max_width = 'max_width';
+        $card_height = 'card_height';
 
         $select_menu = !empty($instance[$card_menu_option]) ? $instance[$card_menu_option] : '';
         $text_title = !empty($instance[$card_title]) ? $instance[$card_title] : '';
         $number_of_cards = !empty($instance[$card_number]) ? $instance[$card_number] : '2';
         $max_width = !empty($instance[$card_max_width]) ? $instance[$card_max_width] : '';
+        $item_height = !empty($instance[$card_height]) ? $instance[$card_height] : '150';
 
         if(empty($select_menu)){
             return;
@@ -39,7 +41,15 @@ class Class_Widget_Apolo_Cards extends \WP_Widget{
             $number_of_cards = 2;
         }
 
+         if(!is_numeric($item_height)){
+            $item_height = '150';
+        }
+
         if(!is_numeric($number_of_cards)){
+            $max_width = '';
+        }
+
+        if(!is_numeric($max_width)){
             $max_width = '';
         }
 
@@ -48,7 +58,8 @@ class Class_Widget_Apolo_Cards extends \WP_Widget{
         <div class="apolo-widget-card">
                 <div class="wrapper-card">
                     <?= !empty($text_title) ? '<h2 class="text-title">'. $text_title . '</h2>' : ''?>
-                    <div class="wrapper-card-content" style="--columns: <?= $number_of_cards ?>;<?= !empty($max_width) ? 'max-width: ' . $max_width . 'px' : '' ?>">
+                    <div class="wrapper-card-content"
+                    style="--columns: <?= $number_of_cards ?>;<?= !empty($max_width) ? 'max-width: ' . $max_width . 'px' : '' ?><?= !empty($item_height) ? '--height: ' . $item_height . 'px' : '' ?>">
                         <?php
                         if(!empty($select_menu)){
                             wp_nav_menu(array(
@@ -71,11 +82,13 @@ class Class_Widget_Apolo_Cards extends \WP_Widget{
         $card_title = 'card_title';
         $card_number = 'card_number';
         $card_max_width = 'max_width';
+        $card_height = 'card_height';
 
         $number_of_cards = !empty($instance[$card_number]) ? $instance[$card_number] : '2';
         $selected_option = !empty($instance[$card_menu_option]) ? $instance[$card_menu_option] : '';
         $card_text = !empty($instance[$card_title]) ? $instance[$card_title] : '';
         $max_width = !empty($instance[$card_max_width]) ? $instance[$card_max_width] : '';
+        $item_height = !empty($instance[$card_height]) ? $instance[$card_height] : '150';
 
         $args = ['taxonomy' => 'nav_menu',
             'orderby' => 'id',
@@ -100,6 +113,9 @@ class Class_Widget_Apolo_Cards extends \WP_Widget{
             <label for="<?php echo $this->get_field_id($card_max_width); ?>"><?php _e('Tamanho máximo do container:', 'text_domain'); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id($card_max_width); ?>" name="<?php echo $this->get_field_name($card_max_width); ?>" type="number" value="<?= $max_width ?>">
 
+            <label for="<?php echo $this->get_field_id($card_height); ?>"><?php _e('Altura do card:', 'text_domain'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id($card_height); ?>" name="<?php echo $this->get_field_name($card_height); ?>" type="number" value="<?= $item_height ?>">
+
             <label><?php _e('Selecione o menu a ser exibido.', 'text_domain'); ?></label>
             <select class="widefat" id="<?php echo $this->get_field_id($card_menu_option); ?>" name="<?php echo $this->get_field_name($card_menu_option); ?>">
                 <?php foreach ($choices as  $key => $choice){
@@ -116,12 +132,14 @@ class Class_Widget_Apolo_Cards extends \WP_Widget{
         $card_title = 'card_title';
         $card_number = 'card_number';
         $card_max_width = 'max_width';
+        $card_height = 'card_height';
 
         $instance = array();
         $instance[$card_menu_option] = (!empty($new_instance[$card_menu_option])) ? sanitize_key($new_instance[$card_menu_option]) : '';
         $instance[$card_title] = (!empty($new_instance[$card_title])) ? sanitize_key($new_instance[$card_title]) : '';
         $instance[$card_number] = (!empty($new_instance[$card_number])) ? sanitize_key($new_instance[$card_number]) : '2';
         $instance[$card_max_width] = (!empty($new_instance[$card_max_width])) ? sanitize_key($new_instance[$card_max_width]) : '';
+        $instance[$card_height] = (!empty($new_instance[$card_height])) ? sanitize_key($new_instance[$card_height]) : '';
 
         return $instance;
     }
