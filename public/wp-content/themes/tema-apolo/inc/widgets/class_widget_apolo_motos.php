26bb68049bdd44f6bfd5fb4cfdc427421e5f6f21
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-11-22 20:20:01 -0300

    Corrige o widget das motos

--- public/wp-content/themes/tema-apolo/inc/widgets/class_widget_apolo_motos.php
+++ public/wp-content/themes/tema-apolo/inc/widgets/class_widget_apolo_motos.php
@@ -1,14 +1,17 @@
 <?php
 
-function registrar_widget_motos() {
+function registrar_widget_motos()
+{
     register_widget('Class_Widget_Apolo_Motos');
 }
 
 add_action('widgets_init', 'registrar_widget_motos');
 
-class Class_Widget_Apolo_Motos extends \WP_Widget {
+class Class_Widget_Apolo_Motos extends \WP_Widget
+{
 
-    function __construct() {
+    function __construct()
+    {
         parent::__construct(
             'motos',
             __('Motos', 'text_domain'),
@@ -18,13 +21,25 @@ class Class_Widget_Apolo_Motos extends \WP_Widget {
         );
     }
 
-    public function widget($args, $instance) {
-    }
+    public function widget($args, $instance)
+    {
+        $post_per_page = !empty($instance['widget_number_motos']) ? $instance['widget_number_motos'] : 4;
+        $categoreie = !empty($instance['widget_category_motos']) ? $instance['widget_category_motos'] : [];
 
-    public function form($instance) {
+        echo "<pre>";
+        print_r(new WP_Query([
+                'post_type' => 'page'
+        ]));
+        echo "<pre>";
+        ?>
+
+        <?php
+        }
+
+        public function form($instance) {
         $defaults = array(
-            'widget_number_motos' => 0,
-            'widget_category_motos' => array(),
+        'widget_number_motos' => 0,
+        'widget_category_motos' => array(),
         );
 
         $instance = wp_parse_args((array) $instance, $defaults);
@@ -40,30 +55,39 @@ class Class_Widget_Apolo_Motos extends \WP_Widget {
 
         <p>
         <label for="<?= $widget_number_motos ?>"><?php _e('Número de Motos apresentadas:', 'text_domain'); ?></label>
-        <input class="widefat" id="<?= $widget_number_motos ?>" name="<?= $widget_number_motos . '[]'?>" type="number" value="<?= $value_number_motos ?>">
+        <input class="widefat" id="<?= $widget_number_motos ?>"
+               name="<?= $this->get_field_name('widget_number_motos') ?>" type="number"
+               value="<?= $value_number_motos ?>">
 
-            <label for="<?= $widget_category_motos; ?>"><?php _e('Categoria das Motos:', 'text_domain'); ?></label>
-            <?php foreach ($categories as $value) { ?>
-                <div>
-                    <input type="checkbox" id="<?= $widget_category_motos . '_' . $value['id']; ?>" name="<?= $widget_category_motos . '[]'; ?>" value="<?= $value['id']; ?>" <?php checked(in_array($value['id'], $categories_checked)); ?>>
-                    <label for="<?= $widget_category_motos . '_' . $value['id']; ?>"><?= $value['name']; ?></label>
-                </div>
-            <?php } ?>
+        <label for="<?= $widget_category_motos; ?>"><?php _e('Categoria das Motos:', 'text_domain'); ?></label>
+        <?php foreach ($categories
+
+                       as $value) { ?>
+        <div>
+            <input type="checkbox" id="<?= $widget_category_motos . '_' . $value['id']; ?>"
+                   name="<?= $widget_category_motos . '[]'; ?>"
+                   value="<?= $value['id']; ?>" <?php checked(in_array($value['id'], $categories_checked)); ?>>
+            <label for="<?= $widget_category_motos . '_' . $value['id']; ?>"><?= $value['name']; ?></label>
+        </div>
+    <?php } ?>
         </p>
 
         <?php
     }
 
-    public function update($new_instance, $old_instance) {
+    public function update($new_instance, $old_instance)
+    {
         $instance = array();
-        $instance['widget_number_motos'] = (!empty($new_instance['widget_number_motos'])) ? $new_instance['widget_number_motos'] : 2;
+
+        $instance['widget_number_motos'] = (!empty($new_instance['widget_number_motos']) && is_numeric($new_instance['widget_number_motos'])) ? $new_instance['widget_number_motos'] : 0;
         $instance['widget_category_motos'] = isset($new_instance['widget_category_motos']) ? array_map('intval', $new_instance['widget_category_motos']) : array();
 
         return $instance;
     }
 
-    private function getCategoriesFiltered() {
-        return array_map(function($category) {
+    private function getCategoriesFiltered()
+    {
+        return array_map(function ($category) {
             return [
                 'id' => $category->term_id,
                 'name' => $category->name,
